d96bc788b6c22618bb9c9ae5e2ebcc9731047b4e
Far1ck <salyakhov.farit@gmail.com>
2025-04-09 00:49:29 +0500

    Homework Done

---
+++ src/org/skypro/skyshop/product/producttypes/FixPriceProduct.java
@@ -0,0 +1,26 @@
+package org.skypro.skyshop.product.producttypes;
+
+import org.skypro.skyshop.product.Product;
+
+public class FixPriceProduct extends Product {
+    private static final int FIX_PRICE = 50;
+
+    public FixPriceProduct(String productName) {
+        super(productName);
+    }
+
+    @Override
+    public int getPrice() {
+        return FIX_PRICE;
+    }
+
+    @Override
+    public String toString() {
+        return getProductName() + ": " + FIX_PRICE;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return true;
+    }
+}
